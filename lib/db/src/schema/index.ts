6e1b73a6bec106b94bb46fbf13d7362e77dc8b99
Shahd Ghanem <shahdnasr7543567@gmail.com>
2026-09-01 22:49:33 +0300

    Remove old Replit artifacts and unused legacy folders

--- lib/db/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./users";
-export * from "./patients";
-export * from "./queue";
-export * from "./drugs";
-export * from "./prescriptions";
-export * from "./inventory";
-export * from "./appointments";
